9d2fbc1607c4ba3e39278559aa6be66270d1b310
Shahriar Rubiat <shahriarshafi@gmail.com>
2023-06-12 18:12:59 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,20 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    mySnackbar(context , msg){
+      return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
+    };
+
+    var MyItems = [
+      {"img" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/800px-IMG_Academy_Logo.svg.png" , "title" : ""},
+      {"img" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/800px-IMG_Academy_Logo.svg.png" , "title" : ""},
+      {"img" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/800px-IMG_Academy_Logo.svg.png" , "title" : ""},
+      {"img" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/800px-IMG_Academy_Logo.svg.png" , "title" : ""},
+      {"img" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/800px-IMG_Academy_Logo.svg.png" , "title" : ""},
+      {"img" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/800px-IMG_Academy_Logo.svg.png" , "title" : ""},
+    ];
+
     return Scaffold(
       appBar: AppBar(title: Text("Photo Gallery"),),
       body: SingleChildScrollView(
@@ -33,7 +47,30 @@ class Home extends StatelessWidget{
                 labelText: "Search For Photos" ,
                 border: OutlineInputBorder(),
               ),
-            ),)
+            ),),
+            Container(
+              height: 100,
+              width:  20,
+              child: ListView.builder(itemCount: MyItems.length,itemBuilder : (context , index){
+                return GestureDetector(
+            child: Container(height: 100, width: 100,
+                 child: Image.network(MyItems[index]['img']! , fit: BoxFit.fill,),
+),
+                );
+              }),),
+            Container(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: (){
+                  mySnackbar(context , "Photo Uploaded Successfully");
+                },
+                iconSize: 100,
+                color: Colors.deepPurple,
+                icon: Icon(
+                  Icons.cloud_upload,
+                ),
+              ),
+            )
           ],
         ),
       ),
